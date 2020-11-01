d9f38f21a999aa90784a877f51ea80eeb18a1262
Tai Do <dophutai.2298@gmail.com>
2020-11-01 08:50:02 +0700

    add logo dat ve

--- src/components/home/ReserveCinema/Branch.jsx
+++ src/components/home/ReserveCinema/Branch.jsx
@@ -2,28 +2,16 @@ import React, { Component } from "react";
 
 class Branch extends Component {
   render() {
+    const { cinema } = this.props;
+    const { cinemaInfo } = this.props;
     return (
-      <div className="cinema__section--branch">
-        <div className="cinema__section--logo active">
-          <span className="btn_cgv">
-            <img src="./images/branch1.png" alt />
-          </span>
-        </div>
-        <div className="cinema__section--logo">
-          <span className="btn_galaxy">
-            <img src="./images/branch2.png" alt />
-          </span>
-        </div>
-        <div className="cinema__section--logo">
-          <span className="btn_lotte">
-            <img src="./images/branch3.png" alt />
-          </span>
-        </div>
-        <div className="cinema__section--logo">
-          <span className="btn_bhd">
-            <img src="./images/branch4.png" alt />
-          </span>
-        </div>
+      <div
+        className="cinema__section--logo active"
+        onClick={cinemaInfo.maHeThongRap}
+      >
+        <button>
+          <img src={cinema.logo} alt />
+        </button>
       </div>
     );
   }
--- src/components/home/ReserveCinema/ReserveCinema.jsx
+++ src/components/home/ReserveCinema/ReserveCinema.jsx
@@ -2,19 +2,47 @@ import React, { Component } from "react";
 import Branch from "./Branch";
 import CinemaReserve from "./CinemaReserve";
 import FilmReserve from "./FilmReserve";
+import { connect } from "react-redux";
+import {
+  fetchCinemaInfoInSystem,
+  fetchCinemaSystem,
+} from "../../../redux/actions/cinema.action";
 
 class ReserveCinema extends Component {
+  renderBranch = () => {
+    const { cinemaList } = this.props;
+    const { cinemaInfo } = this.props;
+    console.log(cinemaList.maHeThongRap);
+    return cinemaList.map((cinema, index) => {
+      return (
+        <div key={index}>
+          <Branch cinema={cinema} cinemaInfo={cinemaInfo} />
+        </div>
+      );
+    });
+  };
+  renderCinemaReserse = (id) => {
+    const { cinemaInfo } = this.props;
+
+    return cinemaInfo.map((cinameInfo, index) => {
+      return (
+        <div key={index}>
+          <CinemaReserve cinameInfo={cinameInfo} />
+        </div>
+      );
+    });
+  };
   render() {
     return (
       <section className="container" id="cumrap">
         <div className="cinema__section">
           {/* branch */}
           <div className="scroll scroll1">
-            <Branch />
+            <div className="cinema__section--branch">{this.renderBranch()}</div>
           </div>
           {/* Cinema */}
           <div className="scroll scroll2">
-            <CinemaReserve />
+            {this.renderCinemaReserse(this.props.cinemaList.maHeThongRap)}
           </div>
           {/* Film */}
           <div className="scroll scroll3">
@@ -24,6 +52,16 @@ class ReserveCinema extends Component {
       </section>
     );
   }
+  componentDidMount() {
+    this.props.dispatch(fetchCinemaSystem());
+    this.props.dispatch(fetchCinemaInfoInSystem());
+  }
 }
 
-export default ReserveCinema;
+const mapStateToProps = (state) => {
+  return {
+    cinemaList: state.cinemaReducer.cinemaList,
+    cinemaInfo: state.cinemaReducer.cinemaInfo,
+  };
+};
+export default connect(mapStateToProps)(ReserveCinema);
