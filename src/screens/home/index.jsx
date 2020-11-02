3523dd38c9239a84950d61d75e70ea37775e8fc9
tay tran dinh <trandinhtay161193@gmail.com>
2020-11-02 22:07:54 +0700

    add header+footer

--- src/screens/home/index.jsx
+++ src/screens/home/index.jsx
@@ -5,15 +5,11 @@ import Line from "../../components/home/Line";
 import ListFilm from "../../components/listfilm/ListFilm";
 import News from "../../components/home/News/News";
 import ReserveCinema from "../../components/home/ReserveCinema/ReserveCinema";
-import Footer from "../../components/layout/Footer";
-
-import Header from "../../components/layout/Header";
 
 class HomeScreen extends Component {
   render() {
     return (
       <div>
-        <Header />
         <Carousel />
         <ListFilm />
         <Line />
@@ -21,7 +17,6 @@ class HomeScreen extends Component {
         <Line />
         <News />
         <Advertise />
-        <Footer />
       </div>
     );
   }
